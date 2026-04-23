16ba2e237db8be0e913576abfc8d5aa669f0db39
wwnsok2 <135630815+wwnsok2@users.noreply.github.com>
2026-04-23 16:56:35 +0900

    Time: 0 ms (100%), Space: 8.3 MB (10.51%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,32 +1,7 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        vector<int> bin;
-        int num = 0;
-
-        while(n != 0)
-        {
-            if(n % 2 == 1)
-            {
-                bin.push_back(1);
-            }
-            else
-            {
-                bin.push_back(0);
-            }
-
-            n = n / 2;
-        }
-
-        for(int i=0;i<bin.size();i++)
-        {
-            if(bin[i] == 1)
-            {
-                num += 1;
-            }
-        }
-        
-        return num;
+        return __builtin_popcount(n);
     }
 
 };
