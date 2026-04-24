6e67feea4276c71ed304653da8bbc397c1dcfdff
wwnsok2 <135630815+wwnsok2@users.noreply.github.com>
2026-04-24 10:06:04 +0900

    Time: 3 ms (31.34%), Space: 11.1 MB (20.71%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int len = nums.size();
+        set<vector<int>> s;
+        
+
+        for(int i=0;i<(1<<len);i++)
+        {
+            vector<int> temp;
+            for(int j=0;j<len;j++)
+            {
+                if(i & (1<<j))
+                {
+                    temp.push_back(nums[j]);
+                    sort(temp.begin(),temp.end());
+                }                
+            }
+            s.insert(temp);
+        }
+        vector<vector<int>> result(s.begin(),s.end());
+        return result;
+    }
+};
