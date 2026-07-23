c591bf45915d628cc6036a166a085c841ab3ac54
wwnsok2 <135630815+wwnsok2@users.noreply.github.com>
2026-07-23 15:33:00 +0900

    Time: 0 ms (100%), Space: 9.3 MB (54.53%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int uniquePaths(int m, int n) {
+        dp.resize(m, vector(n,0));
+
+        for(int i=0;i<m;i++)
+        {
+            dp[i][0] = 1;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            dp[0][i] = 1;
+        }
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }   
+        }
+
+        return dp[m-1][n-1];
+    }
+};
